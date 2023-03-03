3ffbcaccc77b166ea518d50fa2f9494b743f2b88
Arktur007 <126821823+Arktur007@users.noreply.github.com>
2023-03-03 19:29:00 +0300

    Add files via upload

---
+++ app/pages/tickets/tickets.ts
@@ -0,0 +1,82 @@
+import {getTicketById, postTicketData} from "@rest/tickets";
+import '@myCss';
+import '@assets/styles/tickets.scss'
+import {initTicketElementTemplate} from "../../templates/ticketInfo";
+import {IVipTicket, TicketType, ITicket} from "../../models/ticket/ticket";
+import {initFooterTitle, initHeaderTitle} from "@services/general/general";
+
+
+let ticketInstance: TicketType ;
+let ticketPostInstance;
+const clientType = "custom";
+
+initApp();
+registerConfirmButton();
+
+function initApp(): void {
+    const ticketData: Promise<IVipTicket[]> = getTicketById<IVipTicket>('someId');
+    ticketData.then((data): void => {
+        ticketInstance = data[0];
+        const ticketName = typeof ticketInstance?.name === "string" ? ticketInstance?.name : '';
+        initHeaderTitle(ticketName, 'h3');
+        initFooterTitle('Туры по всему миру', 'h2');
+        initTicketInfo(ticketInstance);
+    });
+}
+
+function initTicketInfo(ticket: TicketType | IVipTicket) {
+    const targetElement = document.querySelector('.ticket-info');
+
+    const ticketDescription = ticket?.description;
+    const ticketOperator = ticket?.tourOperator;
+    let vipClientType;
+    if ("vipStatus" in ticket) {
+        vipClientType = ticket.vipStatus;
+    }
+
+    const ticketElemsArr: [string, string, string] = [ticketDescription, ticketOperator, vipClientType];
+
+    let ticketElemTemplate;
+
+    ticketElemsArr.forEach((el, i) => {
+        ticketElemTemplate+= initTicketElementTemplate(el, i);
+    });
+
+    targetElement.innerHTML = ticketElemTemplate;
+
+}
+
+function initUserData() {
+const userInfo = document.querySelectorAll('.user-info > p');
+let userInfoObj;
+    userInfo.forEach((el) => {
+    const inputDataName = el.getAttribute('data-name');
+    if (inputDataName) {
+        const inputElems = el.querySelector('input');
+        userInfoObj[inputDataName] = inputElems.value;
+    }
+    });
+
+    console.log('userInfoObj',userInfoObj)
+    return userInfoObj;
+}
+
+function initPostData(data) {
+    initUserData();
+    postTicketData(data).then((data) => {
+        if (data.success) {
+
+        }
+    })
+}
+
+function registerConfirmButton(): void {
+    const targetEl = document.getElementById('accept-order-button');
+    if (targetEl) {
+        targetEl.addEventListener('click', () => {
+            initPostData(ticketPostInstance);
+        });
+    }
+}
+
+
