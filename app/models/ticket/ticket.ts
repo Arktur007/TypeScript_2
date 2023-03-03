b957ca6f063678df11223b85615ee1d23dd2fa3d
Arktur007 <126821823+Arktur007@users.noreply.github.com>
2023-03-03 19:22:28 +0300

    Add files via upload

---
+++ app/models/ticket/ticket.ts
@@ -0,0 +1,27 @@
+
+import {IUser} from "../user/user";
+
+export interface ITicket {
+    description: string,
+    name: string,
+    price: string,
+    tourOperator: string,
+    location: {
+        x: '30.4044',
+        y: '70.45'
+    },
+    hotel: string
+}
+
+export interface IVipTicket extends ITicket {
+    vipNumber: number,
+    vipStatus: string
+}
+
+export type TicketType = ITicket | IVipTicket;
+
+export interface IPostTicketData {
+    ticket: TicketType,
+    user: IUser,
+}
+
